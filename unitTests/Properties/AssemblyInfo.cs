90772107b079e030c8f5833c74a73b9afe4fde18
Victor Milovanov <lostfreeman@gmail.com>
2015-06-03 00:19:28 -0700

    + signing and trivial testing

--- unitTests/Properties/AssemblyInfo.cs
+++ unitTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lost.Rust.Cargo")]
+[assembly: AssemblyTitle("Lost.Rust.Cargo.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lost.Rust.Cargo")]
+[assembly: AssemblyProduct("Lost.Rust.Cargo.UnitTests")]
 [assembly: AssemblyCopyright("Copyright © Victor Milovanov 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8ed6ee1-9b5f-4af6-9ff5-db1e96a0cd79")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,10 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-
-[assembly: InternalsVisibleTo("Lost.Rust.Cargo.UnitTests, PublicKey="
-+"00240000048000009400000006020000002400005253413100040000010001009b53dc69372a0a"
-+"05b5c777fae200736fdeae85c8610a61cfc74ddddf1922592c7b2bf0fed2a17038054e1c7ae2bf"
-+"0a7c03842908752db0ce2a80d939b9d823268b0c95e209450fa03db9462cc0f9a1e8932a93cda4"
-+"69072cfe7590263927771a708489c315c37116591c087a2686bf184542909a8e281b399521c1d8"
-+"6ac2f6c8")]
---
+++ unitTests/RustTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Lost.Rust.Cargo.UnitTests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class RustTests
+	{
+		[TestMethod]
+		public void ValidateIdentifier_NoThrowOnValid()
+		{
+			Lost.Rust.Cargo.Rust.ValidateIdentifier("HelloWorld_42");
+		}
+	}
+}
